62be567725bfdf196ef6841dae55a238a37d37ee
franzejr <franzejr@gmail.com>
2015-12-08 17:23:18 -0300

    Organize files and add rspec

---
+++ spec/micro_active_record/base_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe MicroActiveRecord::CSVAdapter do
+  pending 'write it'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+require 'bundler/setup'
+
+require "#{File.dirname(__FILE__)}/../lib/micro_active_record"
+Bundler.setup
+
+RSpec.configure do |config|
+end
